b0be392d21765db1d4166fde4749f50d96d3cb40
David Shahmatov <david.shahmatov.work@gmail.com>
2026-07-04 10:43:39 +0300

    update data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,18 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'nairo_backend',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
+
   synchronize: false,
 });
+
+export default AppDataSource;
