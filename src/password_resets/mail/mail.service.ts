fd594ae218d34703fd739eb3e9f49f98ae5488a5
David Shahmatov <david.shahmatov.work@gmail.com>
2026-07-05 19:15:08 +0300

    fix/fix_sending_otp_code

--- src/password_resets/mail/mail.service.ts
+++ src/password_resets/mail/mail.service.ts
@@ -4,8 +4,10 @@ import * as nodemailer from 'nodemailer';
 @Injectable()
 export class MailService {
   private transporter = nodemailer.createTransport({
-    service: 'gmail',
-
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD,
